b7acd2e7c254242aaeb2eeaeb463b14a251f0409
Dina <148201960+guldina1122@users.noreply.github.com>
2023-11-19 00:17:40 +0600

    Add files via upload

---
+++ final2/fast_models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from fastDataBase import Base
+
+class Users(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key = True)
+    username = Column(String(50), nullable = False, unique = True)
+    first_name = Column(String(50), nullable = True)
+    last_name = Column(String(50), nullable = True)
+    email = Column(String(120), nullable = False, unique = True)
+    image_file = Column(String(20), nullable = False, default = 'default.jpg')
+    password = Column(String(80), nullable = False)
